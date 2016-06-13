2290b48756c643c370bc0bc32f578596b64cf6e6
wanderer <mjbecze@gmail.com>
2016-06-13 10:15:30 -0400

    init work on  js transcompiler

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
-const opcodes = require('ethereumjs-vm/lib/opcodes')
+const BN = require('bn.js')
+const opcodes = require('./opcodes.js')
 
-function compile (evmCode) {
+// compile segments
+module.exports = function (evmCode) {
+  const initCode = '(get_local $sp)'
+  let wasmCode = initCode
+  let opcodeCount = 0
+  const segments = []
+  for (let i = 0; i < evmCode.length; i++) {
+    const op = opcodes(evmCode[i])
+    let bytes
+    switch (op.name) {
+      case 'JUMP':
+        wasmCode = `(set_local $temp ${wasmCode})
+                        (set_local $sp i32.sub (get_local $temp) (i32.const 4))
+                        (set_local $jump_loc (get_local $temp))
+                        (br $loop)`
+        break
+      case 'JUMPI':
+        wasmCode = `(block 
+                        (set_local $temp ${wasmCode})
+                        (set_local $sp i32.sub (get_local $temp) (i32.const 8))
+                        (set_local $jump_loc (get_local $temp))
+                        (br_if $loop (i64.load (i32.add (get_local $sp) (i32.const 4)))))`
+        break
+      case 'JUMPDEST':
+        segments.push(wasmCode)
+        wasmCode = ''
+        break
+      case 'LOG':
+        bytes = evmCode.slice(i, i + op.number * 8)
+        for (let i = 0; i < bytes.length; i += 8) {
+          const int64 = bytes2int64(bytes.slice(i, i + 8))
+          wasmCode = `(i64.const ${int64})` + wasmCode
+        }
+        i += op.number
+        break
+      case 'PUSH':
+        bytes = evmCode.slice(i, i + op.number * 8)
+        for (let i = 0; i < bytes.length; i += 8) {
+          const int64 = bytes2int64(bytes.slice(i, i + 8))
+          wasmCode = `(i64.const ${int64})` + wasmCode
+        }
+        // padd the remained of the word with 0
+        for (let i = 0; i < 32 - bytes.length; i += 8) {
+          wasmCode = '(i64.const 0)' + wasmCode
+        }
+        i += op.number
+        break
+      case 'DUP':
+        wasmCode = `(i64.const ${op.number})` + wasmCode
+        break
+      case 'SWAP':
+        wasmCode = `(i64.const ${op.number})` + wasmCode
+        break
+    }
+    wasmCode = `(call $${op.name} ${wasmCode})`
+    opcodeCount++
+  }
+  if (wasmCode !== '') {
+    segments.push(wasmCode)
+  }
+  return assmebleSegments(segments)
 }
 
-/**
- * 1) count instuctions for PC
- */
-// function compileSegment (segment) {
-//   const func = new wasm.func()
-//     segment.forEach((op) => {
-//       const wasmEq = conversionTable(op)
-//       func.add(wasmEq)
-//   })
+function assmebleSegments (segments) {
+  return segments[0]
+}
 
-//   func.end()
-// }
-
-const code = require('./tests/add.json').code
-const result = compile(code)
-console.log(result)
-
+// converts 8 bytes into a int 64
+function bytes2int64(bytes) {
+  return new BN(bytes).fromTwos(64).toString()
+}
 // remap code hashes
 // pull original code; if code is not wasm try to convert it. Transpiler will 
 // need to be written in wasm
