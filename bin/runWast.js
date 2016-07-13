c7d27c216eaab107d35b82889fe923431fc3aa4c
wanderer <mjbecze@gmail.com>
2016-07-13 13:51:39 -0400

    fixed runWast

--- bin/runWast.js
+++ bin/runWast.js
@@ -1,27 +1,10 @@
+#!/usr/bin/env node
 const cp = require('child_process')
 const fs = require('fs')
 const path = require('path')
+const Kernal = require('ewasm-kernel')
 
 const loc = path.join(process.cwd(), process.argv[2])
-
-cp.execSync(`../deps/sexpr-wasm-prototype/out/sexpr-wasm  ${loc} -o ./temp.wasm`)
+cp.execSync(`${__dirname}/../deps/sexpr-wasm-prototype/out/sexpr-wasm  ${loc} -o ./temp.wasm`)
 const opsWasm = fs.readFileSync('./temp.wasm')
-const instance = Wasm.instantiateModule(opsWasm, {
-  spectest: {
-    print: print,
-    printMem: printMem
-  }
-})
-
-instance.exports.main()
-
-function print (i) {
-  console.log(i)
-}
-
-function printMem (i) {
-  console.log('--- mem---');
-  const mem = new Uint8Array(instance.exports.memory).slice(0, 128).join('')
-  console.log(mem)
-}
-
+Kernal.codeHandler(opsWasm)
