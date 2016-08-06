2b5e4d7aa5e436fb5d89de679f0812ea9c92bdf0
Alex Beregszaszi <alex@rtfs.hu>
2016-08-06 04:17:48 +0100

    Fix sexpr-wast-prototype location

--- bin/runWast.js
+++ bin/runWast.js
@@ -5,6 +5,6 @@ const path = require('path')
 const Kernal = require('ewasm-kernel')
 
 const loc = path.join(process.cwd(), process.argv[2])
-cp.execSync(`${__dirname}/../deps/sexpr-wasm-prototype/out/sexpr-wasm  ${loc} -o ./temp.wasm`)
+cp.execSync(`${__dirname}/../tools/sexpr-wasm-prototype/out/sexpr-wasm  ${loc} -o ./temp.wasm`)
 const opsWasm = fs.readFileSync('./temp.wasm')
 Kernal.codeHandler(opsWasm)
