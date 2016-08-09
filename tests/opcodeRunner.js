d58f547fd5cc0df01d71894464465a4ab4103639
wanderer <mjbecze@gmail.com>
2016-08-08 20:07:12 -0400

    changed add.json format

--- tests/opcodeRunner.js
+++ tests/opcodeRunner.js
@@ -36,15 +36,15 @@ tape('testing EVM1 Ops', (t) => {
       }
 
       // populate the stack with predefined values
-      test.stack.in.forEach((item, index) => {
+      test.in.stack.forEach((item, index) => {
         item = hexToUint8Array(item)
         setMemory(testInstance, item, index * 32)
       })
 
       // populate the memory
-      if (test.memory) {
-        Object.keys(test.memory.in).forEach((offset) => {
-            let item = test.memory.in[offset]
+      if (test.in.memory) {
+        Object.keys(test.in.memory).forEach((offset) => {
+            let item = test.in.memory[offset]
             offset |= 0
             offset += EVM_MEMORY_OFFSET
             item = hexToUint8Array(item)
@@ -55,22 +55,22 @@ tape('testing EVM1 Ops', (t) => {
       // Runs the opcode. An empty stack must start with the stack pointer at -8.
       // also we have to add 8 to the resulting sp to accommodate for the fact
       // that the sp is pointing to memory segment holding the last stack item
-      let sp = (test.stack.in.length - 1) * 32 
+      let sp = (test.in.stack.length - 1) * 32 
       sp = testInstance.exports[test.op](...(test.params || []), sp) + 32
-      t.equal(sp / 32, test.stack.out.length, 'should have corrent number of items on the stack')
+      t.equal(sp / 32, test.out.stack.length, 'should have corrent number of items on the stack')
       sp = 0
 
       // compare the output stack against the predefined values
-      test.stack.out.forEach((item, index) => {
+      test.out.stack.forEach((item, index) => {
         const expectedItem = hexToUint8Array(item)
         const result = getMemory(testInstance, sp, sp = sp + 32)
         t.equals(result.toString(), expectedItem.toString(), 'should have correct item on stack')
       })
 
       // check the memory
-      if (test.memory) {
-        Object.keys(test.memory.out).forEach((offset) => {
-          test.memory.out[offset].forEach((item, index) => {
+      if (test.out.memory) {
+        Object.keys(test.out.memory).forEach((offset) => {
+          test.out.memory[offset].forEach((item, index) => {
             offset |= 0
             offset += EVM_MEMORY_OFFSET
             const expectedItem = hexToUint8Array(item)
@@ -81,7 +81,7 @@ tape('testing EVM1 Ops', (t) => {
       }
 
       // check for EVM return value
-      if (test.return) {
+      if (test.out.return) {
         const expectedItem = hexToUint8Array(test.return)
         const result = testEnvironment.returnValue
         t.equals(result.toString(), expectedItem.toString(), 'should have correct return value')
