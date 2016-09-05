8b76b08f62e2915ca89b0c30886c515f30a69015
Alex Beregszaszi <alex@rtfs.hu>
2016-09-06 00:04:14 +0100

    Use new sha3 with memset

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const depMap = new Map([
   ['LOG', ['memusegas', 'check_overflow']],
   ['JUMPI', ['check_overflow']],
   ['JUMP', ['check_overflow']],
-  ['SHA3', ['memusegas', 'bswap_m256', 'bswap_i64', 'check_overflow', 'keccak', 'memcpy']],
+  ['SHA3', ['memusegas', 'bswap_m256', 'bswap_i64', 'check_overflow', 'keccak', 'memcpy', 'memset']],
   ['CALL', ['memusegas', 'check_overflow']],
   ['CREATE', ['memusegas', 'check_overflow']],
   ['RETURN', ['memusegas', 'check_overflow']]
