050337cf32d260268165ea3a6715337e88076287
Alex Beregszaszi <alex@rtfs.hu>
2017-12-21 02:38:13 +0000

    Add C++ module to convert wast to wasm

---
+++ evm2wasm.cpp
@@ -0,0 +1,43 @@
+#include "wasm-binary.h"
+#include "wasm-s-parser.h"
+
+using namespace std;
+
+namespace {
+
+string wast2wasm(string input, bool debug = false) {
+  wasm::Module wasm;
+
+  try {
+    if (debug) std::cerr << "s-parsing..." << std::endl;
+    wasm::SExpressionParser parser(const_cast<char*>(input.c_str()));
+    wasm::Element& root = *parser.root;
+    if (debug) std::cerr << "w-parsing..." << std::endl;
+    wasm::SExpressionWasmBuilder builder(wasm, *root[0]);
+  } catch (wasm::ParseException& p) {
+    p.dump(std::cerr);
+    wasm::Fatal() << "error in parsing input";
+  }
+
+  // FIXME: perhaps call validate() here?
+
+  if (debug) std::cerr << "binarification..." << std::endl;
+  wasm::BufferWithRandomAccess buffer(debug);
+  wasm::WasmBinaryWriter writer(&wasm, buffer, debug);
+  writer.write();
+
+  if (debug) std::cerr << "writing to output..." << std::endl;
+
+  ostringstream output;
+  buffer.writeTo(output);
+
+  if (debug) std::cerr << "Done." << std::endl;
+  
+  return output.str();
+}
+
+}
+
+int main(int argc, char **argv) {
+  cout << wast2wasm("(module (func $test (i64.const 1)))") << endl;
+}
