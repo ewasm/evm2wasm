25f3ccc4bf4c7f979a087311add09f69ed75508a
Alex Beregszaszi <alex@rtfs.hu>
2018-01-21 17:36:55 +0000

    Add wast2wasm tool

--- include/evm2wasm.h
+++ include/evm2wasm.h
@@ -2,4 +2,5 @@
 
 #include <string>
 
+std::string wast2wasm(const std::string& input, bool debug = false);
 std::string evm2wasm(const std::string& input);
---
+++ tools/wast2wasm/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+
+#include <evm2wasm.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    string input;
+    while (!cin.eof())
+    {
+        string tmp;
+        // NOTE: this will read until EOF or NL
+        getline(cin, tmp);
+        input.append(tmp);
+        input.append("\n");
+    }
+
+    if (!input.size())
+        return 1;
+
+    string output = wast2wasm(input);
+
+    if (!output.size())
+        return 1;
+
+    cout << output << endl;
+
+    return 0;
+}
