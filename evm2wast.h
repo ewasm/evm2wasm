968019a2f620913bf08878a4140249b7086e25cc
Silur <abrahamendre9@gmail.com>
2018-01-25 15:30:30 +0100

    rebase&squash

--- evm2wast.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EVM2WAST_H
-#define __EVM2WAST_H
-#include <stdlib.h>
-#ifdef __cplusplus
-extern "C" int evm2wast(char *evm_code, size_t len, char **wast_code, size_t *wast_size);
-#endif
-#endif
--- gadgets.h
+++
@@ -1,5 +0,0 @@
-#ifndef __EVM2WASM_GADGETS_H
-#define __EVM2WASM_GADGETS_H
-extern int gadget_count;
-extern const char *gadgets[];
-#endif
