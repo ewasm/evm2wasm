ff58adb94bab0af6a6433d8f3ab7fe3bb7079417
Silur <abrahamendre9@gmail.com>
2018-02-03 18:52:46 +0100

    sexpression fixes

--- libs/evm2wasm/evm2wast.c
+++ libs/evm2wasm/evm2wast.c
@@ -53,159 +53,159 @@ enum op_nums {
 };
 
 struct opcode opcodes[] = {
-	[STOP] = {"STOP", 0,  0, 0},
-	[ADD] = {"ADD", 3, 2, 1},
-	[MUL] = {"MUL", 5, 2, 1},
-	[SUB] = {"SUB", 3, 2, 1},
-	[DIV] = {"DIV", 5, 2, 1},
-	[SDIV] = {"SDIV", 5, 2, 1},
-	[MOD] = {"MOD", 5, 2, 1},
-	[SMOD] = {"SMOD", 5, 2, 1},
-	[ADDMOD] = {"ADDMOD", 8, 3, 1},
-	[MULMOD] = {"MULMOD", 8, 3, 1},
-	[EXP] = {"EXP", 10, 2, 1},
-	[SIGNEXTEND] = {"SIGNEXTEND", 5, 2, 1},
+	[STOP] = {"STOP", 0,  0, 0, 0},
+	[ADD] = {"ADD", 3, 2, 1, 0},
+	[MUL] = {"MUL", 5, 2, 1, 0},
+	[SUB] = {"SUB", 3, 2, 1, 0},
+	[DIV] = {"DIV", 5, 2, 1, 0},
+	[SDIV] = {"SDIV", 5, 2, 1, 0},
+	[MOD] = {"MOD", 5, 2, 1, 0},
+	[SMOD] = {"SMOD", 5, 2, 1, 0},
+	[ADDMOD] = {"ADDMOD", 8, 3, 1, 0},
+	[MULMOD] = {"MULMOD", 8, 3, 1, 0},
+	[EXP] = {"EXP", 10, 2, 1, 0},
+	[SIGNEXTEND] = {"SIGNEXTEND", 5, 2, 1, 0},
 
-	[LT] = {"LT", 3, 2, 1},
-	[GT] = {"GT", 3, 2, 1},
-	[SLT] = {"SLT", 3, 2, 1},
-	[SGT] = {"SGT", 3, 2, 1},
-	[EQ] = {"EQ", 3, 2, 1},
-	[ISZERO] = {"ISZERO", 3, 1, 1},
-	[AND] = {"AND", 3, 2, 1},
-	[OR] = {"OR", 3, 2, 1},
-	[XOR] = {"XOR", 3, 2, 1},
-	[NOT] = {"NOT", 3, 1, 1},
-	[BYTE] = {"BYTE", 3, 2, 1},
+	[LT] = {"LT", 3, 2, 1, 0},
+	[GT] = {"GT", 3, 2, 1, 0},
+	[SLT] = {"SLT", 3, 2, 1, 0},
+	[SGT] = {"SGT", 3, 2, 1, 0},
+	[EQ] = {"EQ", 3, 2, 1, 0},
+	[ISZERO] = {"ISZERO", 3, 1, 1, 0},
+	[AND] = {"AND", 3, 2, 1, 0},
+	[OR] = {"OR", 3, 2, 1, 0},
+	[XOR] = {"XOR", 3, 2, 1, 0},
+	[NOT] = {"NOT", 3, 1, 1, 0},
+	[BYTE] = {"BYTE", 3, 2, 1, 0},
 
 	// 0x20 range - crypto
-	[SHA3] = {"SHA3", 30, 2, 1},
+	[SHA3] = {"SHA3", 30, 2, 1, 0},
 
 	// 0x30 range - closure state
-	[ADDRESS] = {"ADDRESS", 2, 0, 1},
+	[ADDRESS] = {"ADDRESS", 2, 0, 1, 0},
 	[BALANCE] = {"BALANCE", 400, 1, 1, "$callback_128"},
-	[ORIGIN] = {"ORIGIN", 2, 0, 1},
-	[CALLER] = {"CALLER", 2, 0, 1},
-	[CALLVALUE] = {"CALLVALUE", 2, 0, 1},
-	[CALLDATALOAD] = {"CALLDATALOAD", 3, 1, 1},
-	[CALLDATASIZE] = {"CALLDATASIZE", 2, 0, 1},
-	[CALLDATACOPY] = {"CALLDATACOPY", 3, 3, 0},
+	[ORIGIN] = {"ORIGIN", 2, 0, 1, 0},
+	[CALLER] = {"CALLER", 2, 0, 1, 0},
+	[CALLVALUE] = {"CALLVALUE", 2, 0, 1, 0},
+	[CALLDATALOAD] = {"CALLDATALOAD", 3, 1, 1, 0},
+	[CALLDATASIZE] = {"CALLDATASIZE", 2, 0, 1, 0},
+	[CALLDATACOPY] = {"CALLDATACOPY", 3, 3, 0, 0},
 	[CODESIZE] = {"CODESIZE", 2, 0, 1, "$callback_32"},
 	[CODECOPY] = {"CODECOPY", 3, 3, 0, "$callback"},
-	[GASPRICE] = {"GASPRICE", 2, 0, 1},
+	[GASPRICE] = {"GASPRICE", 2, 0, 1, 0},
 	[EXTCODESIZE] = {"EXTCODESIZE", 700, 1, 1, "$callback_32"},
 	[EXTCODECOPY] = {"EXTCODECOPY", 700, 4, 0, "$callback"},
-	[RETURNDATASIZE] = {"RETURNDATASIZE", 2, 0, 1},
-	[RETURNDATACOPY] = {"RETURNDATACOPY", 3, 3, 0},
+	[RETURNDATASIZE] = {"RETURNDATASIZE", 2, 0, 1, 0},
+	[RETURNDATACOPY] = {"RETURNDATACOPY", 3, 3, 0, 0},
 
 	// '0x40' range - block operations
 	[BLOCKHASH] = {"BLOCKHASH", 20, 1, 1, "$callback_256"},
-	[COINBASE] = {"COINBASE", 2, 0, 1},
-	[TIMESTAMP] = {"TIMESTAMP", 2, 0, 1},
-	[NUMBER] = {"NUMBER", 2, 0, 1},
-	[DIFFICULTY] = {"DIFFICULTY", 2, 0, 1},
-	[GASLIMIT] = {"GASLIMIT", 2, 0, 1},
+	[COINBASE] = {"COINBASE", 2, 0, 1, 0},
+	[TIMESTAMP] = {"TIMESTAMP", 2, 0, 1, 0},
+	[NUMBER] = {"NUMBER", 2, 0, 1, 0},
+	[DIFFICULTY] = {"DIFFICULTY", 2, 0, 1, 0},
+	[GASLIMIT] = {"GASLIMIT", 2, 0, 1, 0},
 
 	// 0x50 range - 'storage' and execution
-	[POP] = {"POP", 2, 1, 0},
-	[MLOAD] = {"MLOAD", 3, 1, 1},
-	[MSTORE] = {"MSTORE", 3, 2, 0},
-	[MSTORE8] = {"MSTORE8", 3, 2, 0},
+	[POP] = {"POP", 2, 1, 0, 0},
+	[MLOAD] = {"MLOAD", 3, 1, 1, 0},
+	[MSTORE] = {"MSTORE", 3, 2, 0, 0},
+	[MSTORE8] = {"MSTORE8", 3, 2, 0, 0},
 	[SLOAD] = {"SLOAD", 200, 1, 1, "$callback_256"},
 	[SSTORE] = {"SSTORE", 0, 2, 0, "$callback"},
-	[JUMP] = {"JUMP", 8, 1, 0},
-	[JUMPI] = {"JUMPI", 10, 2, 0},
-	[PC] = {"PC", 2, 0, 1},
-	[MSIZE] = {"MSIZE", 2, 0, 1},
-	[GAS] = {"GAS", 2, 0, 1},
-	[JUMPDEST] = {"JUMPDEST", 1, 0, 0},
+	[JUMP] = {"JUMP", 8, 1, 0, 0},
+	[JUMPI] = {"JUMPI", 10, 2, 0, 0},
+	[PC] = {"PC", 2, 0, 1, 0},
+	[MSIZE] = {"MSIZE", 2, 0, 1, 0},
+	[GAS] = {"GAS", 2, 0, 1, 0},
+	[JUMPDEST] = {"JUMPDEST", 1, 0, 0, 0},
 
 	// 0x60, range
-	[PUSH1] = {"PUSH", 3, 0, 1},
-	[PUSH2] = {"PUSH", 3, 0, 1},
-	[PUSH3] = {"PUSH", 3, 0, 1},
-	[PUSH4] = {"PUSH", 3, 0, 1},
-	[PUSH5] = {"PUSH", 3, 0, 1},
-	[PUSH6] = {"PUSH", 3, 0, 1},
-	[PUSH7] = {"PUSH", 3, 0, 1},
-	[PUSH8] = {"PUSH", 3, 0, 1},
-	[PUSH9] = {"PUSH", 3, 0, 1},
-	[PUSH10] = {"PUSH", 3, 0, 1},
-	[PUSH11] = {"PUSH", 3, 0, 1},
-	[PUSH12] = {"PUSH", 3, 0, 1},
-	[PUSH13] = {"PUSH", 3, 0, 1},
-	[PUSH14] = {"PUSH", 3, 0, 1},
-	[PUSH15] = {"PUSH", 3, 0, 1},
-	[PUSH16] = {"PUSH", 3, 0, 1},
-	[PUSH17] = {"PUSH", 3, 0, 1},
-	[PUSH18] = {"PUSH", 3, 0, 1},
-	[PUSH19] = {"PUSH", 3, 0, 1},
-	[PUSH20] = {"PUSH", 3, 0, 1},
-	[PUSH21] = {"PUSH", 3, 0, 1},
-	[PUSH22] = {"PUSH", 3, 0, 1},
-	[PUSH23] = {"PUSH", 3, 0, 1},
-	[PUSH24] = {"PUSH", 3, 0, 1},
-	[PUSH25] = {"PUSH", 3, 0, 1},
-	[PUSH26] = {"PUSH", 3, 0, 1},
-	[PUSH27] = {"PUSH", 3, 0, 1},
-	[PUSH28] = {"PUSH", 3, 0, 1},
-	[PUSH29] = {"PUSH", 3, 0, 1},
-	[PUSH30] = {"PUSH", 3, 0, 1},
-	[PUSH31] = {"PUSH", 3, 0, 1},
-	[PUSH32] = {"PUSH", 3, 0, 1},
+	[PUSH1] = {"PUSH", 3, 0, 1, 0},
+	[PUSH2] = {"PUSH", 3, 0, 1, 0},
+	[PUSH3] = {"PUSH", 3, 0, 1, 0},
+	[PUSH4] = {"PUSH", 3, 0, 1, 0},
+	[PUSH5] = {"PUSH", 3, 0, 1, 0},
+	[PUSH6] = {"PUSH", 3, 0, 1, 0},
+	[PUSH7] = {"PUSH", 3, 0, 1, 0},
+	[PUSH8] = {"PUSH", 3, 0, 1, 0},
+	[PUSH9] = {"PUSH", 3, 0, 1, 0},
+	[PUSH10] = {"PUSH", 3, 0, 1, 0},
+	[PUSH11] = {"PUSH", 3, 0, 1, 0},
+	[PUSH12] = {"PUSH", 3, 0, 1, 0},
+	[PUSH13] = {"PUSH", 3, 0, 1, 0},
+	[PUSH14] = {"PUSH", 3, 0, 1, 0},
+	[PUSH15] = {"PUSH", 3, 0, 1, 0},
+	[PUSH16] = {"PUSH", 3, 0, 1, 0},
+	[PUSH17] = {"PUSH", 3, 0, 1, 0},
+	[PUSH18] = {"PUSH", 3, 0, 1, 0},
+	[PUSH19] = {"PUSH", 3, 0, 1, 0},
+	[PUSH20] = {"PUSH", 3, 0, 1, 0},
+	[PUSH21] = {"PUSH", 3, 0, 1, 0},
+	[PUSH22] = {"PUSH", 3, 0, 1, 0},
+	[PUSH23] = {"PUSH", 3, 0, 1, 0},
+	[PUSH24] = {"PUSH", 3, 0, 1, 0},
+	[PUSH25] = {"PUSH", 3, 0, 1, 0},
+	[PUSH26] = {"PUSH", 3, 0, 1, 0},
+	[PUSH27] = {"PUSH", 3, 0, 1, 0},
+	[PUSH28] = {"PUSH", 3, 0, 1, 0},
+	[PUSH29] = {"PUSH", 3, 0, 1, 0},
+	[PUSH30] = {"PUSH", 3, 0, 1, 0},
+	[PUSH31] = {"PUSH", 3, 0, 1, 0},
+	[PUSH32] = {"PUSH", 3, 0, 1, 0},
+	
+	[DUP1] = {"DUP", 3, 0, 1, 0},
+	[DUP2] = {"DUP", 3, 0, 1, 0},
+	[DUP3] = {"DUP", 3, 0, 1, 0},
+	[DUP4] = {"DUP", 3, 0, 1, 0},
+	[DUP5] = {"DUP", 3, 0, 1, 0},
+	[DUP6] = {"DUP", 3, 0, 1, 0},
+	[DUP7] = {"DUP", 3, 0, 1, 0},
+	[DUP8] = {"DUP", 3, 0, 1, 0},
+	[DUP9] = {"DUP", 3, 0, 1, 0},
+	[DUP10] = {"DUP", 3, 0, 1, 0},
+	[DUP11] = {"DUP", 3, 0, 1, 0},
+	[DUP12] = {"DUP", 3, 0, 1, 0},
+	[DUP13] = {"DUP", 3, 0, 1, 0},
+	[DUP14] = {"DUP", 3, 0, 1, 0},
+	[DUP15] = {"DUP", 3, 0, 1, 0},
+	[DUP16] = {"DUP", 3, 0, 1, 0},
 
-	[DUP1] = {"DUP", 3, 0, 1},
-	[DUP2] = {"DUP", 3, 0, 1},
-	[DUP3] = {"DUP", 3, 0, 1},
-	[DUP4] = {"DUP", 3, 0, 1},
-	[DUP5] = {"DUP", 3, 0, 1},
-	[DUP6] = {"DUP", 3, 0, 1},
-	[DUP7] = {"DUP", 3, 0, 1},
-	[DUP8] = {"DUP", 3, 0, 1},
-	[DUP9] = {"DUP", 3, 0, 1},
-	[DUP10] = {"DUP", 3, 0, 1},
-	[DUP11] = {"DUP", 3, 0, 1},
-	[DUP12] = {"DUP", 3, 0, 1},
-	[DUP13] = {"DUP", 3, 0, 1},
-	[DUP14] = {"DUP", 3, 0, 1},
-	[DUP15] = {"DUP", 3, 0, 1},
-	[DUP16] = {"DUP", 3, 0, 1},
+	[SWAP1] = {"SWAP", 3, 0, 0, 0},
+	[SWAP2] = {"SWAP", 3, 0, 0, 0},
+	[SWAP3] = {"SWAP", 3, 0, 0, 0},
+	[SWAP4] = {"SWAP", 3, 0, 0, 0},
+	[SWAP5] = {"SWAP", 3, 0, 0, 0},
+	[SWAP6] = {"SWAP", 3, 0, 0, 0},
+	[SWAP7] = {"SWAP", 3, 0, 0, 0},
+	[SWAP8] = {"SWAP", 3, 0, 0, 0},
+	[SWAP9] = {"SWAP", 3, 0, 0, 0},
+	[SWAP10] = {"SWAP", 3, 0, 0, 0},
+	[SWAP11] = {"SWAP", 3, 0, 0, 0},
+	[SWAP12] = {"SWAP", 3, 0, 0, 0},
+	[SWAP13] = {"SWAP", 3, 0, 0, 0},
+	[SWAP14] = {"SWAP", 3, 0, 0, 0},
+	[SWAP15] = {"SWAP", 3, 0, 0, 0},
+	[SWAP16] = {"SWAP", 3, 0, 0, 0},
 
-	[SWAP1] = {"SWAP", 3, 0, 0},
-	[SWAP2] = {"SWAP", 3, 0, 0},
-	[SWAP3] = {"SWAP", 3, 0, 0},
-	[SWAP4] = {"SWAP", 3, 0, 0},
-	[SWAP5] = {"SWAP", 3, 0, 0},
-	[SWAP6] = {"SWAP", 3, 0, 0},
-	[SWAP7] = {"SWAP", 3, 0, 0},
-	[SWAP8] = {"SWAP", 3, 0, 0},
-	[SWAP9] = {"SWAP", 3, 0, 0},
-	[SWAP10] = {"SWAP", 3, 0, 0},
-	[SWAP11] = {"SWAP", 3, 0, 0},
-	[SWAP12] = {"SWAP", 3, 0, 0},
-	[SWAP13] = {"SWAP", 3, 0, 0},
-	[SWAP14] = {"SWAP", 3, 0, 0},
-	[SWAP15] = {"SWAP", 3, 0, 0},
-	[SWAP16] = {"SWAP", 3, 0, 0},
-
-	[LOG1] = {"LOG", 375, 2, 0},
-	[LOG2] = {"LOG", 375, 2, 0},
-	[LOG3] = {"LOG", 375, 2, 0},
-	[LOG4] = {"LOG", 375, 2, 0},
-	[LOG5] = {"LOG", 375, 2, 0},
+	[LOG1] = {"LOG", 375, 2, 0, 0},
+	[LOG2] = {"LOG", 375, 2, 0, 0},
+	[LOG3] = {"LOG", 375, 2, 0, 0},
+	[LOG4] = {"LOG", 375, 2, 0, 0},
+	[LOG5] = {"LOG", 375, 2, 0, 0},
 
 	// '0xf0' range - closures
 	[CREATE] = {"CREATE", 32000, 3, 1, "$callback_160"},
 	[CALL] = {"CALL", 700, 7, 1, "$callback_32"},
 	[CALLCODE] = {"CALLCODE", 700, 7, 1, "$callback_32"},
-	[RETURN] = {"RETURN", 0, 2, 0},
+	[RETURN] = {"RETURN", 0, 2, 0, 0},
 	[DELEGATECALL] = {"DELEGATECALL", 700, 6, 1, "$callback"},
-	[STATICCALL] = {"STATICCALL", 700, 6, 1},
-	[REVERT] = {"REVERT", 0, 2, 0},
+	[STATICCALL] = {"STATICCALL", 700, 6, 1, 0},
+	[REVERT] = {"REVERT", 0, 2, 0, 0},
 
 	// '0x70', range - other
-	[INVALID] = {"INVALID", 0, 0, 0},
-	[SELFDESTRUCT] = {"SELFDESTRUCT", 5000, 1, 0}
+	[INVALID] = {"INVALID", 0, 0, 0, 0},
+	[SELFDESTRUCT] = {"SELFDESTRUCT", 5000, 1, 0, 0}
 };
 
 
@@ -231,24 +231,24 @@ static int64_t bytes2long(const char *bytes)
 	}
 	return ret;
 }
-static char *padleft(char *bytes, int curr_len, int len)
+static char *padleft(const char *bytes, int curr_len, int len)
 {
 	if(curr_len > len)
 	{
 		fprintf(stderr, "pad failed to prevent memory overlap\n");
 		return 0;
 	}
-	char *ret = calloc(len, 1);
+	char *ret = calloc(len+1, 1);
 	if(!ret) return 0;
-	ret = memcpy(ret+(len-curr_len)-1, bytes, curr_len);
+	memcpy(ret+(len-curr_len), bytes, curr_len);
 
 	return ret;
 }
 
 /*static char *slice(char *bytes, int len)
 {
-	char *ret = malloc(sizeof(len));
-	ret = memcpy(ret, bytes, len);
+	char *ret = malloc(len+1);
+	memcpy(ret, bytes, len);
 	return ret;
 }*/
 
@@ -283,21 +283,21 @@ static char *build_jump_map(size_t *segments, size_t len)
 		segments[i], i, wasm);
 	}
 	size_t s = strlen(wasm) + 358;
-	wasm = realloc(wasm, s);
+	wasm = safe_realloc(wasm, s);
 	if(wasm == 0) goto err;
 	snprintf(wasm + strlen(wasm), s,
 	"(block $0 (if (i32.eqz (get_global $init)) (then (set_global $init (i32.const 1)) (br $0)) (else (if (i32.eq (get_global $cb_dest) (i32.const 0)) (then %s) (else $cb_dest get_global $cb_dest (set_global $cb_dest (i32.const 0)) (br_table $0 ${brTable}))))))", wasm);
 	free(brtable);
 	return wasm;
 err:
-	perror("Falied to allocate memory for assemble_segments");
+	perror("Falied to allocate memory for jump map building");
 	return 0;
 }
 
 static char *assemble_segments(size_t *segments, size_t len)
 {
 	char *wasm = build_jump_map(segments, len);
-	char *ret = malloc(len*(8+digits(len)+strlen(wasm)+1) + 200);
+	char *ret = calloc(len*(8+digits(len)+strlen(wasm)+1) + 200, 1);
 	if(ret == 0) goto err;
 	strcat(ret, "(func $main (export \"main\") (local $jump_dest i32) (set_local $jump_dest (i32.const -1)) (block $done (loop $loop ");
 	unsigned int i;
@@ -305,9 +305,8 @@ static char *assemble_segments(size_t *segments, size_t len)
 	{
 		sprintf(ret+strlen(ret), "(block $%d %s ))", i+1, wasm);
 	}
-	strcat(ret+strlen(ret), "))");
+	strcat(ret, "))");
 	free(wasm);
-	free(ret);
 	return ret;
 err:
 	perror("Falied to allocate memory for assemble_segments");
@@ -333,7 +332,7 @@ static char *add_stack_check(char **segment)
 		if(!check) goto err;
 		snprintf(check, strlen(template_low)+32, template_low,  (-stack_low*32)-32);
 	}
-	char *ret = malloc(strlen(check) + strlen(*segment)+1);
+	char *ret = calloc(strlen(check) + strlen(*segment)+1, 1);
 	sprintf(ret, "%s%s", check, *segment);
 	stack_high = 0;
 	stack_low = 0;
@@ -367,7 +366,7 @@ static inline void end_segment(char **wast, char **segment)
 	add_metering(wast, segment);
 }
 
-static size_t next_jump_dest(char *evm_code, size_t len, size_t i)
+static size_t next_jump_dest(const char *evm_code, size_t len, size_t i)
 {
 	for(; i<len; i++)
 	{
@@ -387,14 +386,14 @@ char *build_module(const char *wast)
 	{
 		gadgets_len += strlen(gadgets[i]);
 	}
-	char *ret = malloc(strlen(wast) + gadgets_len+1024);
+	char *ret = calloc(strlen(wast) + gadgets_len+1024, 1);
 	if(!ret) return 0;
-	ret = strcat(ret, "(module ");
+	strcat(ret, "(module ");
 	for(i=0; i<gadget_count; i++)
 	{
-		ret = strcat(ret, gadgets[i]);
+		strcat(ret, gadgets[i]);
 	}
-	ret = strcat(ret, "(global $cb_dest (mut i32) (i32.const 0))\
+	strcat(ret, "(global $cb_dest (mut i32) (i32.const 0))\
 			(global $sp (mut i32) (i32.const -32))\
 			(global $init (mut i32) (i32.const 0))\
 \
@@ -410,19 +409,19 @@ char *build_module(const char *wast)
 	    (elem ");
 	for(i=0; i<cb_len; i++)
 	{
-		ret = strcat(ret, cb_array[i]);
+		strcat(ret, cb_array[i]);
 	}
-	ret = strcat(ret, "))\n");
-	ret = realloc(ret, strlen(ret+strlen(wast)));
+	strcat(ret, "))\n");
+	ret = realloc(ret, strlen(ret)+strlen(wast)+1);
 	if(!ret) return 0;
-	ret = strcat(ret, wast);
+	strcat(ret, wast);
 	return ret;
 }
 int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_size)
 {
 	size_t i;
-	char *wast_code = malloc(1);
-	char *segment = malloc(1);
+	char *wast_code = calloc(1, 1);
+	char *segment = calloc(1, 1);
 	unsigned char has_jump = 0;
 	size_t *jumps = 0;
 	size_t jumps_len = 0;
@@ -432,7 +431,7 @@ int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_siz
 	{
 		struct opcode op = opcodes[(int)evm_code[i]];
 #define append_segment(template) { \
-				segment = realloc(segment, strlen(segment)+strlen(template)+1);\
+				segment = safe_realloc(segment, strlen(segment)+strlen(template)+1);\
 				if(!segment) goto err;\
 				segment = strcat(segment, template);\
 }
@@ -488,7 +487,7 @@ int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_siz
 				break;
 
 			case JUMPDEST:
-				end_segment(wast_code, segment);
+				end_segment(&wast_code, &segment);
 				jumps = realloc(jumps, jumps_len);
 				jumps[jumps_len++] = i;
 				gas_count = 1;
@@ -496,7 +495,7 @@ int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_siz
 			
 			case GAS:
 				append_segment("(call $GAS)\n");
-				add_metering(wast_code, segment);
+				add_metering(&wast_code, &segment);
 				break;
 
 			case LOG1:
@@ -582,26 +581,28 @@ int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_siz
 				{
 					char *bytes = padleft(evm_code+1, evm_code[i]-0x5f, 32);
 					if(!bytes) goto err;
-					int bytes_rounded = (evm_code[i-1] + 7) >> 3;
-					char *push = 0;
+					int bytes_rounded = (evm_code[i] + 7) >> 3;
+					char *push = calloc(1, 1);
 					int q;
 					for(q=0; q<4-bytes_rounded; q++)
 					{
-						push = realloc(push, (q+1)*32); // FIXME segfault
+						push = safe_realloc(push, (q+1)*32); // FIXME segfault
 						if(!push) goto err;
 						sprintf(push, "(i64.const 0)%s", push);
 					}
+
+					int64_t i64;
 					for(; q<4; q++)
 					{
-						int64_t i64 = bytes2long(slice(bytes+(q*8), q*8+8));
-						push = realloc(push, (q+1)*32);
+						i64 = bytes2long(bytes+(q*8));
+						push = safe_realloc(push, (q+1)*32);
 						if(!push) goto err;
-						sprintf(push, "%s (i64.const %ld)", push, i64);
-
+						snprintf(push, (q+1)*32, "%s (i64.const %ld)", push, i64);
 					}
-					format_segment("(call $PUSH %s)", 13+strlen(push), push);
+					format_segment("(call $PUSH %s)", 14+strlen(push)+digits(i64), push);
 					i+=(size_t)evm_code[i]-0x5f;
 					free(push);
+					free(bytes);
 				}
 				break;
 
@@ -664,18 +665,20 @@ int evm2wast(const char *evm_code, size_t len, char **wast_ret, size_t *wast_siz
 		if(op.cb != 0)
 		{
 			format_segment("(set_global $cb_dest (i32.const %ld)) (br $done))", 72, jumps_len+1);
-			jumps = realloc(jumps, (jumps_len+1)*sizeof(size_t));
+			jumps = safe_realloc(jumps, (jumps_len+1)*sizeof(size_t));
 			if(!jumps) goto err;
 			jumps[jumps_len++] = i;
 		}
 	}
-	end_segment(wast_code, segment);
+	end_segment(&wast_code, &segment);
 	char *with_jumps = assemble_segments(jumps, jumps_len);
-	wast_code = realloc(wast_code, strlen(wast_code) + strlen(with_jumps));
-	if(wast_code == 0) goto err;
-	wast_code = strcat(with_jumps, wast_code);
-	wast_code = strcat(wast_code, "))");
-	wast_code = build_module(wast_code);
+	//wast_code = safe_realloc(wast_code, strlen(wast_code) + strlen(with_jumps)+3);
+	//if(wast_code == 0) goto err;
+	char *temp = calloc(strlen(wast_code) + strlen(with_jumps)+3, 1);
+	sprintf(temp, "%s%s)", with_jumps, wast_code);
+	free(wast_code);
+	wast_code = build_module(temp);
+	free(temp);
 	free(segment);
 	free(with_jumps);
 	free(jumps);
