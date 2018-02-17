0c99e5c78639931374f45db2b4bbd8a30a5ec41f
Jared Wasinger <j-wasinger@hotmail.com>
2018-02-17 15:52:45 +0000

    simplify code, use sync fs functions

--- bin/evm2wasm.js
+++ bin/evm2wasm.js
@@ -5,23 +5,6 @@ const ethUtil = require('ethereumjs-util')
 const argv = require('minimist')(process.argv.slice(2))
 const fs = require('fs')
 
-//read EVM bytecode from a file
-function readEVM (file) {
-  return new Promise((resolve, reject) => {
-    if (file) {
-      fs.readFile(file, (err, data) => {
-        if (err) {
-          reject(err)
-        }
-
-        resolve(data) // strip newline
-      })
-    } else {
-      reject("no file")
-    }
-  })
-}
-
 //convert evm bytecode to WASM or WAST
 function convert (bytecode, wast) {
   return new Promise((resolve, reject) => {
@@ -55,44 +38,40 @@ function convert (bytecode, wast) {
 }
 
 function storeOrPrintResult(output, outputFile) {
-  return new Promise((resolve, reject) => {
-    if (typeof(output) !== 'string') {
-      output = output.buffer
-    }
-    if (outputFile) {
-      fs.writeFile(outputFile, output, (err) => {
-        if (err) {
-          reject(err)
-        }
-      })
-    } else {
-      console.log(output)
-      resolve()
-    }
-  })
+  if (typeof(output) !== 'string') {
+    output = output.buffer
+  }
+
+  if (outputFile) {
+    fs.writeFileSync(outputFile, output)
+  } else {
+    console.log(Buffer.from(output).toString('binary'))
+  }
 }
 
-(async () => {
-  let outputFile = argv.o ? argv.o : undefined
-  let wast = argv.wast !== undefined
-  let file = argv.e ? argv.e : undefined
 
-  let bytecode
+let outputFile = argv.o ? argv.o : undefined
+let wast = argv.wast !== undefined
+let file = argv.e ? argv.e : undefined
 
-  try {
-    if (!file) {
-      if (argv._.length > 0) {
-        bytecode = argv._[0]  
-      } else {
-        throw("must provide evm bytecode file or supply bytecode as a non-named argument")
-      }
+let bytecode
+
+try {
+  if (!file) {
+    if (argv._.length > 0) {
+      bytecode = argv._[0]  
     } else {
-      bytecode = await readEVM(file)
+      throw("must provide evm bytecode file or supply bytecode as a non-named argument")
     }
-
-    let result = await convert(bytecode, wast)
-    await storeOrPrintResult(result, outputFile)
-  } catch (err) {
-    console.error("Error: " + err)
+  } else {
+    bytecode = fs.readFileSync(file)
   }
-}) ()
+
+  convert(bytecode, wast).then((result) => {
+    storeOrPrintResult(result, outputFile)
+  }).catch((err) => {
+    throw(err)
+  })
+} catch (err) {
+  console.error("Error: " + err)
+}
