cbcc564416b84e6f8ab99addf134fdd9909cf117
Alex Beregszaszi <alex@rtfs.hu>
2018-08-15 03:51:27 +0100

    Fix some cases of discarding signedness

--- libs/evm2wasm/evm2wasm.cpp
+++ libs/evm2wasm/evm2wasm.cpp
@@ -21,7 +21,7 @@ using namespace fmt::literals;
 
 namespace {
 
-bool nibble2value(char input, unsigned& output) {
+bool nibble2value(unsigned input, unsigned& output) {
   if (input >= '0' && input <= '9') {
     output = input - '0';
     return true;
@@ -48,7 +48,7 @@ vector<uint8_t> hexstring2vector(const string& input) {
   vector<uint8_t> ret;
   for (size_t i = 0; i <= len - 2; i += 2) {
     unsigned lo, hi;
-    if (!nibble2value(input[i], hi) || !nibble2value(input[i + 1], lo))
+    if (!nibble2value(unsigned(input[i]), hi) || !nibble2value(unsigned(input[i + 1]), lo))
       return vector<uint8_t>{};
     ret.push_back(static_cast<uint8_t>((hi << 4) | lo));
   }
@@ -168,7 +168,7 @@ string evm2wast(const vector<uint8_t>& evmCode, bool stackTrace, bool useAsyncAP
 
     for (size_t pc = 0; pc < evmCode.size(); pc++)
     {
-        auto opint = evmCode[pc];
+        uint8_t opint = evmCode[pc];
         auto op = opcodes(opint);
 
         // creates a stack trace
@@ -272,9 +272,10 @@ string evm2wast(const vector<uint8_t>& evmCode, bool stackTrace, bool useAsyncAP
                 bytes.insert(bytes.begin(), 32 - op.number, 0);
             }
 
-            auto bytesRounded = ceil((double)op.number / 8.0);
+            // op.number is an 8bit number, casting to size_t should be safe here
+            size_t bytesRounded = size_t(ceil((double)op.number / 8.0));
             fmt::MemoryWriter push;
-            int q = 0;
+            size_t q = 0;
 
             // pad the remaining of the word with 0
             for (; q < 4 - bytesRounded; q++)
@@ -289,10 +290,10 @@ string evm2wast(const vector<uint8_t>& evmCode, bool stackTrace, bool useAsyncAP
             {
                 //TODO clean this disgusting mess up
 
-                std::reverse(bytes.begin()+q*8, bytes.begin()+q*8+8);
+                std::reverse(bytes.begin() + q * 8, bytes.begin() + q * 8 + 8);
 
                 int64_t int64 = 0;
-                memcpy(&int64, &bytes[q*8], 8);
+                memcpy(&int64, &bytes[q * 8], 8);
 
                 push << "(i64.const {int64})"_format("int64"_a = int64);
             }
@@ -341,11 +342,15 @@ string evm2wast(const vector<uint8_t>& evmCode, bool stackTrace, bool useAsyncAP
             {
                 std::string cbFunc = (*callbackFuncs.find(op.name)).second;
                 auto result = std::find(std::begin(callbackTable), std::end(callbackTable), cbFunc);
-                size_t index = result - std::begin(callbackTable);
+                size_t index;
                 if (result == std::end(callbackTable))
                 {
                     callbackTable.push_back(cbFunc);
                     index = callbackFuncs.size();
+                }
+                else
+                {
+                    index = result - std::begin(callbackTable);
                 }
                 segment << "(call ${opname} (i32.const {index}))\n"_format("opname"_a = opcodeToString(op.name), "index"_a = index);
             }
@@ -598,7 +603,7 @@ std::string opcodeToString(opcodeEnum opcode)
     }
 }
 
-Op opcodes(int op)
+Op opcodes(uint8_t op)
 {
     auto result = codes.find(op);
     std::tuple<opcodeEnum, int, int, int> code;
@@ -632,7 +637,8 @@ Op opcodes(int op)
         break;
 
     default:
-        number = -1;
+        number = 0;
+        break;
     }
 
     return {opcode, std::get<1>(code), std::get<2>(code), std::get<3>(code), number};
